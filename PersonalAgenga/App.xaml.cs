a6ef943a3660f0cdccb228a3f9ff9c72d5c7bb75
DIANA-TEODORA ARDELEAN <diana.teodora.ardelean@stud.ubbcluj.ro>
2020-12-10 09:47:02 +0200

    Add project files.

---
+++ PersonalAgenga/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PersonalAgenga.Data;
+using System.IO;
+
+namespace PersonalAgenga
+{
+    public partial class App : Application
+
+    {
+        static AgendaDatabase database;
+
+        public static AgendaDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   AgendaDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
+    
+
+        public App()
+        {
+            InitializeComponent();
+        MainPage = new NavigationPage(new AgendaPage());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
